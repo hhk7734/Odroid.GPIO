0fbb1081a8fc443ad4a19f12a4a03761f7146bf1
Hyeonki Hong <hhk7734@gmail.com>
2019-08-28 17:12:11 +0900

    Add libwiringpi-dev, remove odroid-config

--- install_requires.sh
+++ install_requires.sh
@@ -1,34 +1,22 @@
 #!/bin/sh
 
+set -e
+
 is_installed()
 {
     [ -n "$(command -v "$1" 2>/dev/null)" ]
 }
 
-if ! (  ( is_installed gcc ) && ( is_installed git ) && ( is_installed swig ) ); then
-    echo "\033[31m""\n============================================================\n""\033[0m"
+if ! (  ( is_installed gcc ) && ( is_installed git ) && ( is_installed swig ) && ( is_installed gpio ) ); then
+    echo "\033[31m""\nSwig, odroid-wiringpi, and etc\n""\033[0m"
+    add-apt-repository ppa:hardkernel/ppa
     apt update
-    apt install -y build-essential git swig
-fi
-
-if ! is_installed gpio; then
-    echo "\033[31m""\n============================================================\n""\033[0m"
-    if [ -e /tmp/wiringPi ]; then
-        rm -r /tmp/wiringPi
-    fi
-    git clone https://github.com/hardkernel/wiringPi /tmp/wiringPi &&
-    cd /tmp/wiringPi &&
-    ./build
+    apt install -y build-essential swig odroid-wiringpi libwiringpi-dev
 fi
 
 if ! is_installed odroid-config; then
-    echo "\033[31m""\n============================================================\n""\033[0m"
-    if [ -e /tmp/odroid-config ]; then
-        rm -r /tmp/odroid-config
-    fi
-    git clone https://github.com/hhk7734/odroid-config /tmp/odroid-config &&
-    cd /tmp/odroid-config &&
-    ./install.sh
+    echo "\033[31m""\nodroid-config installation\n""\033[0m"
+    echo "Not yet supported."
 fi
 
 echo "\033[31m""\n============================================================\n""\033[0m"
